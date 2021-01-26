c64ee7167382017565ffaaba95f4a508e7233c88
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-01-26 18:18:52 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-export default {
+const config = {
   API_ENDPOINT:
     process.env.REACT_APP_API_ENDPOINT ||
     `https://obscure-plains-76776.herokuapp.com/api/`,
   API_TOKEN: process.env.REACT_APP_API_TOKEN
 };
+
+export default config;
