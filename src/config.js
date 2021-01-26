0faa9f5a57970aa136fe9ac25b4320fd818646ba
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-01-26 18:24:14 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  API_ENDPOINT:
-    process.env.REACT_APP_API_ENDPOINT ||
-    `https://obscure-plains-76776.herokuapp.com/api/`,
+  API_ENDPOINT: `https://obscure-plains-76776.herokuapp.com/api/`,
   API_TOKEN: process.env.REACT_APP_API_TOKEN
 };
 
