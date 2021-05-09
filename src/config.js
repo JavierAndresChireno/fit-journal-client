c76dcf6b9aa4c6de9168d0d8c87a98e1357ed6a2
Yefenny Chireno <yefenny300@icloud.com>
2021-05-09 17:13:29 -0400

    change api-endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_ENDPOINT: `https://obscure-plains-76776.herokuapp.com/api/`,
+  API_ENDPOINT: `https://fit-journal-api.herokuapp.com/api/`,
   API_TOKEN: process.env.REACT_APP_API_TOKEN
 };
 
